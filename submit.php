21d6805afdd4bda0cce82237a5fc448ccc7f8a35
Domingas Mendes Duarte <dduarte@mag.com.br>
2023-08-18 21:24:52 -0300

    contact message configuration

--- submit.php
+++ submit.php
@@ -4,16 +4,46 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $message = $_POST["msg"];
 
-    $to = "domingasduarte95@gmail.com";
-    $subject = "New Contact Form Submission";
-    $headers = "From: $email";
-    
-    $mailBody = "Name: $name\nEmail: $email\nMessage: $message";
+    // Proteção contra ataques maliciosos
+    $name = filter_var($name, FILTER_SANITIZE_STRING);
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $message = filter_var($message, FILTER_SANITIZE_STRING);
 
-    if (mail($to, $subject, $mailBody, $headers)) {
-        echo "Message sent successfully!";
+    // Validar reCAPTCHA (chave secreta)
+    $recaptchaSecret = 'SUA_CHAVE_SECRETA_DO_RECAPTCHA';
+    $recaptchaResponse = $_POST['g-recaptcha-response'];
+    $url = "https://www.google.com/recaptcha/api/siteverify";
+    $data = [
+        'secret' => $recaptchaSecret,
+        'response' => $recaptchaResponse
+    ];
+    $options = [
+        'http' => [
+            'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method' => 'POST',
+            'content' => http_build_query($data)
+        ]
+    ];
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    $recaptchaResult = json_decode($result);
+
+    if ($recaptchaResult->success) {
+        // Enviar o e-mail
+        $to = "domingasduarte95@gmail.com";
+        $subject = "New Contact Form Submission";
+        $headers = "From: $email";
+        $mailBody = "Name: $name\nEmail: $email\nMessage: $message";
+
+        if (mail($to, $subject, $mailBody, $headers)) {
+            header("Location: confirmation.html");
+            exit;
+        } else {
+            echo "Message could not be sent.";
+        }
     } else {
-        echo "Message could not be sent.";
+        echo "reCAPTCHA validation failed.";
     }
 }
 ?>
+
